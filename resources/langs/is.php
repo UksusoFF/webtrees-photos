632c2d3db65ad5268975a26309096f815d0c3d16
Gudjon Sigurdsson <100283838+gudjonsi@users.noreply.github.com>
2024-12-02 17:25:08 +0000

    Create is.php
    Icelandic language file created for webtrees photos.

---
+++ resources/langs/is.php
@@ -0,0 +1,11 @@
+<?php
+
+use Fisharebest\Webtrees\I18N;
+
+return [
+    'Photos' => 'Myndir',
+    'Recent photos' => 'Nýlegar myndir',
+    'A list of photos that have been uploaded recently.' => 'Listi yfir myndir sem hefur verið hlaðið upp nýlega.',
+    'There have been no photos found.' => 'Engar myndir fundust.',
+    'Last %s uploaded photos' . I18N::PLURAL . 'Last %s uploaded photos' => 'Síðasta %s upphlaðna myndin' . I18N::PLURAL . 'Síðustu %s upphlöðnu myndirnar',
+];
